d7861e72ba3e5e36cf67209a248a6b1bfdb237e8
chochelius <chochelius@gmail.com>
2021-10-24 01:50:18 -0300

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,298 @@
+// creación de los array
+
+let radiologia = [
+  {
+    hora: "11:00",
+    especialista: "IGNACIO SCHULZ",
+    paciente: "FRANCISCA ROJAS",
+    rut: "9878782-1",
+    prevision: "FONASA",
+  },
+  {
+    hora: "11:30",
+    especialista: "FEDERICO SUBERCASEAUX",
+    paciente: "PAMELA ESTRADA",
+    rut: "15345241-3",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "15:00",
+    especialista: "FERNANDO WURTHZ",
+    paciente: "ARMANDO LUNA",
+    rut: "16445345-9",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "15:30",
+    especialista: "ANA MARIA GODOY",
+    paciente: "MANUEL GODOY",
+    rut: "17666419-0",
+    prevision: "FONASA",
+  },
+  {
+    hora: "16:00",
+    especialista: "PATRICIA SUAZO",
+    paciente: "RAMON ULLOA",
+    rut: "14989389-K",
+    prevision: "FONASA",
+  },
+];
+
+traumatologia = [
+  {
+    hora: "8:00",
+    especialista: "MARIA PAZ ALTUZARRA",
+    paciente: "PAULA SANCHEZ",
+    rut: "15554774-5",
+    prevision: "FONASA",
+  },
+  {
+    hora: "10:00",
+    especialista: "RAUL ARAYA",
+    paciente: "ANGÉLICA NAVAS",
+    rut: "15444147-9",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "10:30",
+    especialista: "MARIA ARRIAGADA",
+    paciente: "ANA KLAPP",
+    rut: "17879423-9",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "11:00",
+    especialista: "ALEJANDRO BADILLA",
+    paciente: "FELIPE MARDONES",
+    rut: "1547423-6",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "11:30",
+    especialista: "CECILIA BUDNIK",
+    paciente: "DIEGO MARRE",
+    rut: "16554741-K",
+    prevision: "FONASA",
+  },
+  {
+    hora: "12:00",
+    especialista: "ARTURO CAVAGNARO",
+    paciente: "CECILIA MENDEZ",
+    rut: "9747535-8",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "12:30",
+    especialista: "ANDRES KANACRI",
+    paciente: "MARCIAL SUAZO",
+    rut: "11254785-5",
+    prevision: "ISAPRE",
+  },
+];
+
+dental = [
+  {
+    hora: "8:30",
+    especialista: "ANDREA ZUÑIGA",
+    paciente: "MARCELA RETAMAL",
+    rut: "11123425-6",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "11:00",
+    especialista: "MARIA PIA ZAÑARTU",
+    paciente: "ANGEL MUÑOZ",
+    rut: "9878789-2",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "11:30",
+    especialista: "SCARLETT WITTING",
+    paciente: "MARIO KAST",
+    rut: "7998789-5",
+    prevision: "FONASA",
+  },
+  {
+    hora: "13:00",
+    especialista: "FRANCISCO VON TEUBER",
+    paciente: "KARIN FERNANDEZ",
+    rut: "18887662-K",
+    prevision: "FONASA",
+  },
+  {
+    hora: "13:30",
+    especialista: "EDUARDO VIÑUELA",
+    paciente: "HUGO SANCHEZ",
+    rut: "17665461-4",
+    prevision: "FONASA",
+  },
+  {
+    hora: "14:00",
+    especialista: "RAQUEL VILLASECA",
+    paciente: "ANA SEPULVEDA",
+    rut: "14441281-0",
+    prevision: "ISAPRE",
+  },
+];
+
+// agregando elementos a traumatología  
+
+
+traumatologia.push(
+  {
+    hora: "09:00",
+    especialista: "RENÉ POBLETE",
+    paciente: "ANA GELLONA",
+    rut: "13123329-7",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "09:30",
+    especialista: "MARIA SOLAR",
+    paciente: "RAMIRO ANDRADE",
+    rut: "12221451-K",
+    prevision: "FONASA",
+  },
+  {
+    hora: "10:00",
+    especialista: "RAUL LOYOLA",
+    paciente: "CARMEN ISLA",
+    rut: "10112348-3",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "10:30",
+    especialista: "ANTONIO LARENAS",
+    paciente: "PABLO LOAYZA",
+    rut: "13453234-1",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "12:00",
+    especialista: "MATIAS ARAVENA",
+    paciente: "SUSANA POBLETE",
+    rut: "14345656-6",
+    prevision: "FONASA",
+  },
+);
+
+
+//nuevas horas de traumatología con las añadiduras
+
+document.write(`
+<br>
+<h2> Horas Traumatología </h2>
+<br>
+<table class="table">
+<thead>
+  <tr>
+    <th scope="col">Hora</th>
+    <th scope="col">Especialista</th>
+    <th scope="col">Paciente</th>
+    <th scope="col">RUT</th>
+    <th scope="col">Prevision</th>
+    </thead>
+  </tr>`);
+
+traumatologia.forEach((item) =>
+  document.write(` <tr>
+<th scope="row">${item.hora}</th>
+<td>${item.especialista}</td>
+<td>${item.paciente}</td>
+<td>${item.rut}</td>
+<td>${item.prevision}</td>
+</tr>`)
+);
+
+document.write(`</table>`);
+
+let radiologiaV2 = radiologia.pop();
+let radiologiaV3 = radiologia.shift();
+
+//nuevas horas de Radiología con la primera y la última eliminada.
+
+document.write(`
+<br>
+<h2> Horas Radiología </h2>
+<br>
+<table class="table">
+<thead>
+  <tr>
+    <th scope="col">Hora</th>
+    <th scope="col">Especialista</th>
+    <th scope="col">Paciente</th>
+    <th scope="col">RUT</th>
+    <th scope="col">Prevision</th>
+    </thead>
+  </tr>`);
+
+radiologia.forEach((item) =>
+  document.write(` <tr>
+<th scope="row">${item.hora}</th>
+<td>${item.especialista}</td>
+<td>${item.paciente}</td>
+<td>${item.rut}</td>
+<td>${item.prevision}</td>
+</tr>`)
+);
+
+document.write(`</table>`);
+
+
+//horas de dental nuevo formato
+
+document.write(`
+<br>
+<h2> Horas Dental </h2>
+<br>`);
+
+dental.forEach((item) =>
+  document.write(` <tr>
+<p>${item.hora} -  ${item.especialista} - ${item.paciente} - ${item.rut} - ${item.prevision} </p> <br>`)
+);
+
+//todas las horas
+
+const todasLasHoras = radiologia.concat(dental, traumatologia);
+
+
+//pacientes fonasa e isapre
+  const pacientesFonasa = todasLasHoras.filter( todasLasHoras => todasLasHoras.prevision == "FONASA" );
+
+  const pacientesIsapre = todasLasHoras.filter( todasLasHoras => todasLasHoras.prevision == "ISAPRE" );
+
+document.write(`
+<br>
+<h2> Todos Los pacientes </h2>
+<br>`);
+
+todasLasHoras.forEach((item) =>
+  document.write(` <tr>
+<p>${item.paciente}</p> <br>`)
+);
+
+
+document.write(`
+<br>
+<h2> Pacientes Isapre </h2>
+<br>`);
+
+
+pacientesIsapre.forEach((item) =>
+  document.write(` <tr>
+<p>${item.paciente} - ${item.prevision} </p> <br>`)
+);
+
+document.write(`
+<br>
+<h2> Pacientes Fonasa </h2>
+<br>`);
+
+
+
+
+
+pacientesFonasa.forEach((item) =>
+  document.write(` <tr>
+<p>${item.paciente} - ${item.prevision} </p> <br>`)
+);
